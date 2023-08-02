488d7395d71c49ae55be1951fca624449d77cb7e
Aditya Kumar <theadityakumardeveloper@gmail.com>
2023-08-02 12:56:49 +0000

    Updated Footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -98,10 +98,13 @@ export default function Footer() {
             <Link href={"/instructor"}>Instructor/Author</Link>
           </li>
           <li>
-            <Link href={"https://datawealthy.com/about"}>About Us</Link>
+            <Link href={"https://datawealthy.com/about"} target="_blank">About Us</Link>
           </li>
           <li>
-            <Link href={"https://datawealthy.com/contact"}>Contact Us</Link>
+            <Link href={"https://datawealthy.com/contact"} target="_blank">Contact Us</Link>
+          </li>
+          <li>
+            <Link href={"https://github.com/datawealthy"} target="_blank">GitHub</Link>
           </li>
         </ul>
         <ul>
@@ -115,6 +118,10 @@ export default function Footer() {
           </li>
         </ul>
       </div>
+      <hr className="container" />
+      <div className="container" style={{marginBottom: 20, marginTop: 20}}>
+        <p className="s">&copy; Made by <b><Link href="https://github.com/adityajideveloper">adityajideveloper</Link></b> and brought to you by <b><Link href="https://datawealthy.com">Data Wealthy</Link>.</b></p>
+    </div>
     </footer>
   );
 }
